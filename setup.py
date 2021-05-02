23ee8eeca292f8442f13cd6c6bfbea43e9bd3be2
Sergey Panfilov <sergray@gmail.com>
2021-05-02 20:13:59 +0200

    Remove Python 3.5 and add 3.9 to CI

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="CLI tool for symmetric encryption/decryption using cryptography.fernet",
     install_requires=requirements,
